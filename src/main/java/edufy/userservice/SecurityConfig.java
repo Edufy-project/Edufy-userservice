42eeab6101ed95fcfc4cc6df0e072b750ace52aa
samuel.funck <samuel.funck@outlook.com>
2025-11-25 12:26:43 +0100

    security config added
    -Can receive relay tokens from gate.
    -Can read roles from jwt token, both realm and client.
    -Currently allows all calls to be made by any authenticated user, nomather the role.

---
+++ src/main/java/edufy/userservice/SecurityConfig.java
@@ -0,0 +1,69 @@
+package edufy.userservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtAuthenticationConverter jwtAuthConverter = new JwtAuthenticationConverter();
+        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(this :: extractKeyCloakRoles);
+        return jwtAuthConverter;
+    }
+
+    private Collection<GrantedAuthority> extractKeyCloakRoles(Jwt jwt){
+        Collection<GrantedAuthority>  authorities = new ArrayList<>();
+
+        //realm roles
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+            authorities.addAll(roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList())
+            );
+        }
+
+        //client roles
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess != null && resourceAccess.containsKey("user-service")) {
+            Map<String, Object> client = (Map<String, Object>) resourceAccess.get("user-service");
+            Collection<String> roles = (Collection<String>) client.get("roles");
+            authorities.addAll(roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList())
+            );
+        }
+        
+        return authorities;
+    }
+
+}
